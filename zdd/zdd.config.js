04864c2077d44085e17e79ff9ab8587c21bc2a77
Zach Dyer <zach.dyer@zachdyerdesign.com>
2016-03-09 12:57:28 -0600

    Major update

---
+++ zdd/zdd.config.js
@@ -0,0 +1,8 @@
+zdd.config = {
+    "title" : "Caring Clowns for Souls",
+    "baseurl" : "http://caringclownsforsouls.org/",
+    "theme" : "caringclowns",
+    "author" : "Zach Dyer Design",
+    "website" : "http://zachdyerdesign.com",
+    "year" : new Date().getFullYear()
+};
